a9eea685a4fff1bad8c1c29a7a3d2181ee09dae1
Makachai Jacobs <malachai.jacobs15@gmail.com>
2022-01-11 11:58:28 -0700

    slide 1 through 7 finished

---
+++ shopping list/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  shopping list
+//
+//  Created by Malachai Jacobs on 1/10/22.
+//
+
+import Foundation
+class Item
+{
+    var name : String
+    init(name: String)
+    {
+        self.name = name
+    }
+}
--- shopping list/ViewController.swift
+++ shopping list/ViewController.swift
@@ -7,10 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource
+{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+        
+    }
+    
+    @IBOutlet weak var newItemTextfield: UINavigationItem!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
+        
+        let currentItem = items[indexPath.row]
+        cell.textLabel?.text = currentItem.name
+        return cell
+    }
+    
+    var items : [Item] = [ ]
 
-    override func viewDidLoad() {
+
+    @IBAction func whenAddItemButtonPressed(_ sender: Any)
+    {
+        if let newItemName = newItemTextfield.text
+        {
+        let newItem = Item(name: newItemName)
+        items.append(newItem)
+        tableView.reloadData()
+        }
+        }
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad()
+    {
+        
+        let item1 = Item(name: "Milk")
+        let item2 = Item(name: "Eggs")
+        items = [item1, item2]
+        tableView.dataSource = self
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
